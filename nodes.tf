341e79607f3c7462f5ad3b1c091bbd024022dc3e
Max! <admin@cloudydev.net>
2023-08-06 19:52:38 +0200

    Update nodes.tf

--- nodes.tf
+++ nodes.tf
@@ -1,8 +1,7 @@
-/*
 resource "hcloud_server" "control_plane" {
   name        = "control-plane"
   server_type = "cx31"
-  image       = "debian-12"
+  image       = "ubuntu-22"
   location    = "nbg1"
   ssh_keys    = ["max"] 
   user_data = "${file("user-data.yaml")}"
@@ -15,21 +14,3 @@ resource "hcloud_server" "control_plane" {
 output "control_plane_ipv4_address" {
   value = hcloud_server.control_plane.ipv4_address
 }
-
-resource "hcloud_server" "node" {
-  name        = "node0"
-  server_type = "cx31"
-  image       = "debian-12"
-  location    = "nbg1"
-  ssh_keys    = ["max"] 
-  user_data = "${file("user-data.yaml")}"
-  public_net {
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
-}
-
-output "node_ipv4_address" {
-  value = hcloud_server.node.ipv4_address
-}
-*/
