37b319f298bfa392fc1c5356c12ded86f222f7c0
Max! <admin@cloudydev.net>
2023-08-16 19:36:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,20 @@ resource "hcloud_server" "control_plane" {
 output "control_plane_ipv4_address" {
   value = hcloud_server.control_plane.ipv4_address
 }
+
+resource "hcloud_server" "monitoring" {
+  name        = "vpn"
+  server_type = "cx31"
+  image       = "ubuntu-22.04"
+  location    = "nbg1"
+  ssh_keys    = ["max"] 
+  user_data = "${file("monitoring.yaml")}"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
+
+output "control_plane_ipv4_address" {
+  value = hcloud_server.monitoring.ipv4_address
+}
