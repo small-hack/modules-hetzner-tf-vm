6ef4efa9f9f55ac4f1dc77de1c76aec83db6a751
Max! <admin@cloudydev.net>
2023-09-17 10:40:04 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,38 @@ variable "hcloud_token" {
   sensitive = true
 }
 
+variable "cloudflare_account_id" {
+  type = string
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
+
+variable "tunnel_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "github_username" {
+  type = string
+}
+
+variable "ssh_pub_key" {
+  sensitive = true
+}
+
+variable "server_type" {
+  stype = string
+}
+
+variable "hetzner_ssh_key_name" {
+  type = string
+}
